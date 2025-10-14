a156ce342b5ca06a6bd615bc7fd14c64ea58f93a
itsmerit37 <itsmerit37@gmail.com>
2025-10-14 19:53:09 +0530

    has password

--- auth-backend/models/user.model.js
+++ auth-backend/models/user.model.js
@@ -1,18 +1,31 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-
-const userSchema = mongoose.Schema({
-   username : {
-       type: String,
-       unique: true,
-       required: true
-   },
-   password : {
-       type: String,
-       required: true
-   }
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 });
 
+// ✅ Pre-save hook to hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-const userModel = mongoose.model('User', userSchema);
-export default userModel;
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
